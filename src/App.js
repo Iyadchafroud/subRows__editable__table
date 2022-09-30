2da53e0ab872caf1fe004da542f1b20a60cf89f7
CHAFROUD IYED <chafroud.iyad@gmail.com>
2022-09-30 13:54:06 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from "react";
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable react/button-has-type */
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useTable, usePagination, useExpanded } from "react-table";
-
-import makeData from "./makeData";
 
 const Styles = styled.div`
   padding: 1rem;
@@ -47,46 +47,42 @@ const Styles = styled.div`
 // Create an editable cell renderer
 function EditableCell({
   value: initialValue,
-  row: row,
+  row,
   column: { id },
-  updateMyData // This is a custom function that we supplied to our table instance
+  updateMyData, // This is a custom function that we supplied to our table instance
 }) {
-  console.log(row);
-
-  // We need to keep and update the state of the cell normally
   const [value, setValue] = React.useState(initialValue);
 
   const onChange = (e) => {
     setValue(e.target.value);
   };
 
-  // We'll only update the external data when the input is blurred
   const onBlur = () => {
-    updateMyData(row.index, id, value);
+   const rowId=row.id 
+
+    updateMyData(row.index, id, value, row.depth, rowId);
   };
 
-  // If the initialValue is changed external, sync it up with our state
   React.useEffect(() => {
     setValue(initialValue);
   }, [initialValue]);
+  if (id === "Désignation") {
+    return <p>{value}</p>;
+  }
   if (row.subRows.length > 0) {
     return <p>{value}</p>;
   }
+
   if (row.subRows.length === 0) {
     return <input value={value} onChange={onChange} onBlur={onBlur} />;
   }
 }
 
-// Set our editable cell renderer as the default Cell renderer
 const defaultColumn = {
-  Cell: EditableCell
+  Cell: EditableCell,
 };
 
-// Be sure to pass our updateMyData and the skipPageReset option
 function Table({ columns, data, updateMyData, skipPageReset }) {
-  // For this example, we're using pagination to illustrate how to stop
-  // the current page from resetting when our data changes
-  // Otherwise, nothing is different here.
   const {
     getTableProps,
     getTableBodyProps,
@@ -101,21 +97,15 @@ function Table({ columns, data, updateMyData, skipPageReset }) {
     nextPage,
     previousPage,
     setPageSize,
-    state: { pageIndex, pageSize }
+    state: { pageIndex, pageSize },
   } = useTable(
     {
       columns,
       data,
       defaultColumn,
-
-      // use the skipPageReset option to disable page resetting temporarily
       autoResetPage: !skipPageReset,
-      // updateMyData isn't part of the API, but
-      // anything we put into these options will
-      // automatically be available on the instance.
-      // That way we can call this function from our
-      // cell renderer!
-      updateMyData
+
+      updateMyData,
     },
     useExpanded,
     usePagination
@@ -209,94 +199,119 @@ function App() {
           </span>
         ),
         Cell: ({ row }) =>
-          // Use the row.canExpand and row.getToggleRowExpandedProps prop getter
-          // to build the toggle for expanding a row
           row.canExpand ? (
             <span
               {...row.getToggleRowExpandedProps({
                 style: {
-                  // We can even use the row.depth property
-                  // and paddingLeft to indicate the depth
-                  // of the row
-                  paddingLeft: `${row.depth * 2}rem`
-                }
+                  paddingLeft: `${row.depth * 2}rem`,
+                },
               })}
             >
               {row.isExpanded ? "👇" : "👉"}
             </span>
-          ) : null
+          ) : null,
       },
       {
-        Header: "First Name",
-        accessor: "firstName"
+        Header: "Désignation",
+        accessor: "Désignation",
       },
       {
-        Header: "Last Name",
-        accessor: "lastName"
+        Header: "PrixHT",
+        accessor: "PrixHT",
       },
 
       {
-        Header: "Age",
-        accessor: "age"
+        Header: "TVA",
+        accessor: "TVA",
       },
       {
-        Header: "Visits",
-        accessor: "visits"
+        Header: "PrixTTC",
+        accessor: "PrixTTC",
       },
-      {
-        Header: "Status",
-        accessor: "status"
-      },
-      {
-        Header: "Profile Progress",
-        accessor: "progress"
-      }
     ],
     []
   );
-  const [data, setData] = React.useState(makeData(1, 5, 5));
-  console.log({ data });
-
+  const [data, setData] = React.useState([
+    {
+      Désignation: "emporter",
+      PrixHT: "",
+      TVA: "",
+      PrixTTC: "",
+      subRows: [
+        {
+          Désignation: "kiosk",
+          PrixHT: "",
+          TVA: "",
+          PrixTTC: "",
+        },
+        {
+          Désignation: "dfiofsk",
+          PrixHT: "",
+          TVA: "",
+          PrixTTC: "",
+        },
+      ],
+    },
+    {
+      Désignation: "emporter",
+      PrixHT: "",
+      TVA: "",
+      PrixTTC: "",
+      subRows: [
+        {
+          Désignation: "kiosk",
+          PrixHT: "",
+          TVA: "",
+          PrixTTC: "",
+        },
+        {
+          Désignation: "dfiofsk",
+          PrixHT: "",
+          TVA: "",
+          PrixTTC: "",
+        },
+      ],
+    },
+  ]);
   const [originalData] = React.useState(data);
   const [skipPageReset, setSkipPageReset] = React.useState(false);
 
-  // We need to keep the table from resetting the pageIndex when we
-  // Update data. So we can keep track of that flag with a ref.
+  const updateMyData = (rowIndex, columnId, value, deptht, designation) => {
+    // console.log(rowIndex, columnId, deptht, value);
 
-  // When our cell renderer calls updateMyData, we'll use
-  // the rowIndex, columnId and new value to update the
-  // original data
-  const updateMyData = (rowIndex, columnId, value) => {
-    // We also turn on the flag to not reset the page
     setSkipPageReset(true);
-    old;
     setData((old) =>
       old.map((row, index) => {
         if (index === rowIndex) {
-          return {
-            ...old[rowIndex],
-            [columnId]: value
-          };
+          // console.log(row);
+          row.subRows.map((el, indexSub) => {
+            console.log(index, indexSub);
+
+            // if (indexSub === rowIndex) {
+            //     console.log(el.Désignation);
+
+            //     if (designation === row.Désignation) {
+            //         el[columnId] = value;
+            //     }
+            //}
+          });
         }
+
         return row;
       })
     );
   };
 
-  // After data chagnes, we turn the flag back off
-  // so that if data actually changes when we're not
-  // editing it, the page is reset
   React.useEffect(() => {
     setSkipPageReset(false);
   }, [data]);
 
-  // Let's add a data resetter/randomizer to help
-  // illustrate that flow...
   const resetData = () => setData(originalData);
+  const [modal, setModal] = useState(false);
 
+  const toggle = () => setModal(!modal);
   return (
     <Styles>
-      <button onClick={resetData}>Reset Data</button>
       <Table
         columns={columns}
         data={data}
